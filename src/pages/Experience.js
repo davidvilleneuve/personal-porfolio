377a89aef0bc4ba3fe7da0f6f397944fb3c3d089
davidvilleneuve <david.villeneuve.catipon17@hotmail.com>
2022-10-28 03:28:24 -0400

    ADD EXPERIENCE

--- src/pages/Experience.js
+++ src/pages/Experience.js
@@ -6,71 +6,120 @@ import {
 import "react-vertical-timeline-component/style.min.css";
 import SchoolIcon from "@material-ui/icons/School";
 import WorkIcon from "@material-ui/icons/Work";
+import "../styles/Experience.css";
 
 function Experience() {
   return (
     <div className="experience">
       <VerticalTimeline lineColor="#3e497a">
-        <VerticalTimelineElement
-          className="vertical-timeline-element--education"
-          date="2010 - 2014"
-          iconStyle={{ background: "#3e497a", color: "#fff" }}
-          icon={<SchoolIcon />}
-        >
-          <h3 className="vertical-timeline-element-title">
-            My Random High School, Random Place, Random State
-          </h3>
-          <p> High School Diploma</p>
-        </VerticalTimelineElement>
-        <VerticalTimelineElement
-          className="vertical-timeline-element--education"
-          date="2014 - 2018"
-          iconStyle={{ background: "#3e497a", color: "#fff" }}
-          icon={<SchoolIcon />}
-        >
-          <h3 className="vertical-timeline-element-title">
-            My Cool University, Vancouver, British Columbia
-          </h3>
-
-          <h4 className="vertical-timeline-element-subtitle">
-            Bachelor's Degree
-          </h4>
-
-          <p> Computer Science</p>
-        </VerticalTimelineElement>
-        <VerticalTimelineElement
+      <VerticalTimelineElement
+      className="vertical-timeline-element--work"
+      date="2016 - 2017"
+      iconStyle={{ background: "#e9d35b", color: "#fff" }}
+      icon={<WorkIcon />}
+    >
+      <h3 className="vertical-timeline-element-title">
+       Clerk - Couche Tard
+      </h3>
+      <h4 className="vertical-timeline-element-subtitle">
+        Laval, QC
+      </h4>
+      <p>Warehouseman</p>
+    </VerticalTimelineElement>
+      <VerticalTimelineElement
           className="vertical-timeline-element--work"
-          date="2018 - 2020"
+          date="2017"
           iconStyle={{ background: "#e9d35b", color: "#fff" }}
           icon={<WorkIcon />}
         >
           <h3 className="vertical-timeline-element-title">
-            Back End Engineer - Google
+           Warehouseman - ATS Healthcare 
           </h3>
           <h4 className="vertical-timeline-element-subtitle">
-            San Francisco, CA
+            Laval, QC
+          </h4>
+          <p>Warehouseman</p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--education"
+          date="2018 - 2019"
+          iconStyle={{ background: "#3e497a", color: "#fff" }}
+          icon={<SchoolIcon />}
+        >
+          <h3 className="vertical-timeline-element-title">
+          École des métiers de l'aérospatiale de Montréal
+          </h3>
+
+          <h4 className="vertical-timeline-element-subtitle">
+            DEP
+          </h4>
+
+          <p> Cable and Circuit Assembly</p>
+        </VerticalTimelineElement>
+        
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          date="2019"
+          iconStyle={{ background: "#e9d35b", color: "#fff" }}
+          icon={<WorkIcon />}
+        >
+          <h3 className="vertical-timeline-element-title">
+            Electromechanian - Capmatic
+          </h3>
+          <h4 className="vertical-timeline-element-subtitle">
+            Montreal, Qc
           </h4>
           <p>Developed the backend infrastructure for 3 projects.</p>
         </VerticalTimelineElement>
-
         <VerticalTimelineElement
+          className="vertical-timeline-element--education"
+          date="Obtained in 2019"
+          iconStyle={{ background: "#3e497a", color: "#fff" }}
+          icon={<SchoolIcon />}
+        >
+          <h3 className="vertical-timeline-element-title">
+          High school Diploma
+          </h3>
+
+        </VerticalTimelineElement>
+
+        
+        <VerticalTimelineElement
+        className="vertical-timeline-element--education"
+        date="2020 - 2022"
+        iconStyle={{ background: "#3e497a", color: "#fff" }}
+        icon={<SchoolIcon />}
+      >
+        <h3 className="vertical-timeline-element-title">
+        Lasalle College
+        </h3>
+  
+        <h4 className="vertical-timeline-element-subtitle">
+          AEC
+        </h4>
+  
+        <p> Computer Science</p>
+      </VerticalTimelineElement>
+      <VerticalTimelineElement
           className="vertical-timeline-element--work"
-          date="2020 - present"
+          date="2022"
           iconStyle={{ background: "#e9d35b", color: "#fff" }}
           icon={<WorkIcon />}
         >
           <h3 className="vertical-timeline-element-title">
-            Full Stack Engineer - Twitch
+            Web Developer - Commission scolaire de la Seigneurie-des-Mille-Îles
           </h3>
           <h4 className="vertical-timeline-element-subtitle">
-            San Francisco, CA
+           St-Eusatche, Qc
           </h4>
           <p>
             Helped the team launch 2 major features by working both in the front
             end and back end.
           </p>
-        </VerticalTimelineElement>
+        </VerticalTimelineElement> 
       </VerticalTimeline>
+     
+      
     </div>
   );
 }
